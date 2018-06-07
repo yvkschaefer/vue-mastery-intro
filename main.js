acd8a851225f1a494be073cd815a326d07d541db
Kara Schaefer <kara.schaefer@alayacare.com>
2018-06-07 16:10:03 -0400

    1, 2, 3 exercises

---
+++ main.js
@@ -0,0 +1,9 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: './assets/vmSocks-green-onWhite.jpeg',
+        inventory: 8,
+        onSale: true
+    }
+})
