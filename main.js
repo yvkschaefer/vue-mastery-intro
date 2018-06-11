2cd4d6bec32175e79ae85a75fb709553c5d7a517
Kara Schaefer <kara.schaefer@alayacare.com>
2018-06-11 11:42:52 -0400

    show cart

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
 let app = new Vue({
-    el: '#app',
-    data: {
-        product: 'Socks',
-        image: './assets/vmSocks-green-onWhite.jpeg',
-        inventory: 8,
-        onSale: true
+  el: '#app',
+  data: {
+    product: 'Socks',
+    image: './assets/vmSocks-green.jpeg',
+    inventory: 8,
+    onSale: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColour: 'green',
+        variantImage: './assets/vmSocks-green.jpeg'
+      },
+      {
+        variantId: 2235,
+        variantColour: 'blue',
+        variantImage: './assets/vmSocks-blue.jpeg'
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart: function() {
+      this.cart += 1;
+    },
+    changeSockColour: function() {
+      this.image === '';
     }
-})
+  }
+});
